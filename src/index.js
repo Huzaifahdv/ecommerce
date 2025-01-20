c865f3e37074a46b751f0f41f61a3b5ab2ae5a7e
Huzaifahdv <holmzsharlok66@gmail.com>
2025-01-20 23:07:55 +0600

    most sold product

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/style.css'
 import '@fortawesome/fontawesome-free/js/all.min'
 
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(item => new bootstrap.Tooltip(item))
+
+document.querySelectorAll('.add-to-cart-btn').forEach(item => {
+    item.addEventListener("click", () => {
+        alert("أضيف المنتج إلى عربة الشراء")
+    })
+})
 
 console.log("أهلا بكم في متجر عربي")
